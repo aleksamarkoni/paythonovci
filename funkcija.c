3a1438e2b0c2038234495279766ab93cc9f27f66
aleksamarkoni <aleksamarkoni@gmail.com>
2016-10-24 19:30:29 +0200

    Ovo je primer programa u dva fajla

---
+++ funkcija.c
@@ -0,0 +1,3 @@
+int saberiDvaBroja(int a, int b) {
+   return a+b;
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int saberiDvaBroja(int a, int b);
+
+int main() {
+   printf("Sabirak dva broja 3 i 5 je: %d", saberiDvaBroja(3, 5));
+   return 0;
+}
