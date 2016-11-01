3a5dbe6e2898dd88066ab9e6151905eb7ec8f8fb
dusan <dusan.kojcic90@gmail.com>
2016-11-01 22:48:12 +0100

    Reseni problemi u pesicevom zadatku

--- temp.c
+++ temp.c
@@ -13,22 +13,25 @@
 struct istorija {
   float celsius;
   float fahrenheit;
+  int tip;
 };
 struct istorija memorija[20];
-
 int brojac;
 
-void unos_history_celsius(float celsius, float fahrenheit) {
+void unos_history_celsius(float celsius, int tip, float fahrenheit) {
   if ( brojac > 4){
     for ( (brojac) = 1 ; (brojac) < 5; (brojac)++ ){
       memorija[brojac-1].fahrenheit = memorija[brojac].fahrenheit;
-      memorija[brojac-1].celsius = memorija[brojac].celsius; }
+      memorija[brojac-1].celsius = memorija[brojac].celsius;
+      memorija[brojac-1].tip = memorija[brojac].tip;}
       brojac = 4;
       memorija[brojac].celsius = celsius;
+      memorija[brojac].tip = tip;
       memorija[brojac].fahrenheit = fahrenheit;
     }
   else {
     memorija[brojac].celsius = celsius;
+    memorija[brojac].tip = tip;
     memorija[brojac].fahrenheit = fahrenheit;
   }
 }
@@ -42,22 +45,24 @@ return ((PET/DEVET) * (fahrenheit - TRIDESET_DVA));
 }
 
 void konvert_cf() {
+  int tip = 0;
   float celsius;
   float fahrenheit;
   printf("\nUnesite temperaturu u Celzijusima: ");
   scanf("%f", &celsius);
   fahrenheit=konvert_iz_cels_u_farh(celsius);
-  unos_history_celsius(celsius, fahrenheit);
+  unos_history_celsius(celsius, tip, fahrenheit);
 
   printf("Temperatura u Farenhaitima je: %f\n", fahrenheit);
 }
 void konvert_fc() {
+  int tip = 1;
   float celsius;
   float fahrenheit;
   printf("\nUnesite temperaturu u Farenhaitima: ");
   scanf("%f", &fahrenheit);
   celsius = konvert_iz_farh_u_cels(fahrenheit);
-  unos_history_celsius(celsius, fahrenheit);
+  unos_history_celsius(celsius, tip, fahrenheit);
   printf("Temperatura u Celsiusima je: %f\n", celsius);
 }
 void tabela() {
@@ -102,26 +107,33 @@ void prikazi_menu(int *izbor) {
 void history_ispis () {
   int j;
       for(j = 0; j < 5; j++) {
-        printf("%d: %.2f %.2f\n",(j+1),
+        printf("%d: tip %d %.2f C %.2f F\n",(j+1), memorija[j].tip,
          memorija[j].celsius, memorija[j].fahrenheit);}
+         printf("\ntip 0 - iz Celzijusa u Farenhaita\n");
+         printf("tip 1 - iz Farenhaita u Celzijusa\n");
   }
 
 int main() {
   int pogresni_unos = 0;
+  char answer;
   int izbor, izbor2;
   do {
     prikazi_menu(&izbor);
   switch (izbor) {
   case 1:
+  do {
     konvert_cf();
     brojac++;
-    konvert_again1();
+    printf("\nDa li zelite da konvertujete ponovo ? [D/N]\n");
+  } while (scanf(" %c", &answer) == 1 && answer == 'D' || answer == 'd');
     pogresni_unos = 0;
   break;
   case 2:
+  do {
     konvert_fc();
     brojac++;
-    konvert_again2();
+    printf("\nDa li zelite da konvertujete ponovo ? [D/N]\n");
+  } while (scanf(" %c", &answer) == 1 && answer == 'D' || answer == 'd');
     pogresni_unos = 0;
   break;
   case 3:
