ccc9c1b50c332a10dc64c0acedf32e967001c002
zolak94 <n.zotek94@gmail.com>
2017-02-06 19:08:39 +0100

    Dodati pubovi

--- DomaciZolak/main.cpp
+++ DomaciZolak/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "lokal.h"
 #include "kafic.h"
+#include "pub.h"
 
 using namespace std;
 
@@ -53,7 +54,11 @@ void izborKafane(int *izbor) {
 
 int main() {
   Kafic kafic1("Kafic", "Renesansa", "Kralja Aleksandra I Karadjordjevica", "Kafa + Limunada = 120 RSD");
-  Lokal kafic2("Kafic", "Bridge", "Suboticeva BB");
+  Kafic kafic2("Kafic", "Bridge", "Suboticeva BB", "Espreso + Kola = 149 RSD");
+  Kafic kafic3("Kafic", "Ultra Caffe", "Aviv Park", "1. Kafa = 30 RSD \n2. Espreso = 60 RSD \n3. Kola = 80 RSD");
+  Pub pub1("Pub", "Lion", "Bulevar oslobodjenja BB", "Njuske", "Erdinger 199 RSD");
+  Pub pub2("Pub", "London", "Gimnazijska", "Vatrene kise", "Zajecarsko 100 RSD");
+  Pub pub3("Pub", "Zlatna krigla", "Koce Kolarova", "/", "Hobogoblin pivo 289 RSD");
   int izbor;
   izborLokala(&izbor);
   switch(izbor) {
@@ -66,11 +71,14 @@ int main() {
           cout << "Danas je na akciji: " << endl << kafic1.getAkcija() << endl;
         break;
         case 2:
-          cout << kafic2.getVrsta() << " " << kafic2.getIme() << endl;
-          cout << kafic2.getAdresa() << endl;
+          cout << kafic2.getVrstaKafica() << " " << kafic2.getImeKafica() << endl;
+          cout << kafic2.getAdresaKafica() << endl;
+          cout << "Danas je na akciji: " << endl << kafic2.getAkcija() << endl;
         break;
         case 3:
-          //ispisInformacija();
+          cout << kafic3.getVrstaKafica() << " " << kafic3.getImeKafica() << endl;
+          cout << kafic3.getAdresaKafica() << endl;
+          cout << "Happy Hour od 16h do 18h: " << endl << kafic3.getAkcija() << endl;
         break;
         case 0:
           cout << "Hvala sto ste koristili nasu aplikaciju!" << endl;
@@ -82,6 +90,32 @@ int main() {
     break;
     case 2:
       izborPuba(&izbor);
+      switch(izbor) {
+        case 1:
+          cout << pub1.getImePuba() << " " << pub1.getVrstaPuba() << endl;
+          cout << pub1.getAdresaPuba() << endl;
+          cout << "Veceras nastupa bend: " << endl << pub1.getBend() << endl;
+          cout << "Vecerasnja promocija: " << endl << pub1.getPromocija() << endl;
+        break;
+        case 2:
+          cout << pub2.getImePuba() << " " << pub2.getVrstaPuba() << endl;
+          cout << pub2.getAdresaPuba() << endl;
+          cout << "Veceras nastupa bend: " << endl << pub2.getBend() << endl;
+          cout << "Vecerasnja promocija: " << endl << pub1.getPromocija() << endl;
+        break;
+        case 3:
+          cout << pub3.getImePuba() << " " << pub3.getVrstaPuba() << endl;
+          cout << pub3.getAdresaPuba() << endl;
+          cout << "Veceras nastupa bend: " << endl << pub3.getBend() << endl;
+          cout << "Vecerasnja promocija: " << endl << pub3.getPromocija() << endl;
+        break;
+        case 0:
+          cout << "Hvala sto ste koristili nasu aplikaciju!" << endl;
+        return 0;
+        default:
+          cout << "Pogresan izbor" << endl;
+        return 0;
+      }
     break;
     case 3:
       izborKluba(&izbor);
