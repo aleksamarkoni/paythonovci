11c1fab1c70dda6c7dcb58048eeee2581ffdfbc4
dusankojcic <dusan.kojcic90@gmail.com>
2017-03-16 23:30:43 +0100

    Uspeo da ubacim korisnika u kanal i testirao radi

--- networking/server.cpp
+++ networking/server.cpp
@@ -15,7 +15,7 @@
 #include <signal.h>
 #include <pthread.h>
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -25,6 +25,84 @@ using namespace std;
 
 int connected_client[100];
 int i = 0;
+string ispis;
+
+class Korisnik {
+private:
+  int sock;
+  char username[666];
+public:
+  Korisnik() {}
+  Korisnik(int sock, char *username) {
+    setSocket(sock);
+    setUsername(username);
+  }
+  void setSocket(int sock) {
+    this->sock = sock;
+  }
+  void setUsername(char *username) {
+    for (int p = 0; p < strlen(username); p++) {
+      strcpy(this->username, username);
+    }
+  }
+  int getSocket() {
+    return sock;
+  }
+  char getUsername() {
+    return *username;
+  }
+  string printUsername() {
+    for (int p = 0; p < strlen(username); p++) {
+      ispis += username[p];
+    }
+    return ispis;
+  }
+  friend ostream& operator<<(ostream &out, const Korisnik &user);
+};
+
+ostream& operator<<(ostream &out, Korisnik &user) {
+  out << user;
+  return out;
+}
+
+class Kanal {
+private:
+  string ime;
+  vector <Korisnik> korisnici;
+public:
+  Kanal() {}
+  Kanal(string imeKanala) {
+    setKanal(imeKanala);
+  }
+  void setKanal(string imeKanala) {
+    this->ime = imeKanala;
+  }
+  void dodajKorisnika(Korisnik &user) {
+    korisnici.push_back(user);
+  }
+  string getImeKanala() {
+    return this->ime;
+  }
+  vector<Korisnik> getKorisnik() {
+    return this->korisnici;
+  }
+  friend ostream& operator<<(ostream &out, Kanal &kanal);
+  friend ostream& operator<<(ostream &out, vector<Korisnik> &korisnici);
+};
+
+vector<Kanal> kanali;
+
+ostream& operator<<(ostream &out, Kanal &kanal) {
+  out << kanal;
+  return out;
+}
+
+ostream& operator<<(ostream &out, vector<Korisnik> &korisnici) {
+  out << kanali[0].getKorisnik()[0] << endl;
+  return out;
+}
+
+//vector<std::pair<int, Korisnik> > kanali;
 
 //the thread function
 void *connection_handler(void *);
@@ -43,8 +121,7 @@ void *user() {
 }
 
 
-int main(void)
-{
+int main(void) {
 
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
     struct addrinfo hints, *servinfo, *p;
@@ -108,12 +185,12 @@ int main(void)
             perror("accept");
             continue;
         }
-        std::cout << "Username: " << std::endl;
+        //std::cout << "Username: " << std::endl;
 
 
         inet_ntop(their_addr.ss_family,
             get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-        std::cout << "server: got connection from \n" << s << std::endl;
+        //std::cout << "server: got connection from \n" << s << std::endl;
 
         connected_client[i++] = new_fd;
 
@@ -121,48 +198,82 @@ int main(void)
         int *new_sock = (int*) malloc(sizeof (int));
         *new_sock = new_fd;
 
-        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        if( pthread_create( &sniffer_thread , NULL ,
+          connection_handler , (void*) new_sock) < 0)
         {
             perror("could not create thread");
             return 1;
         }
-
         //Now join the thread , so that we dont terminate before the thread
         //pthread_join( sniffer_thread , NULL);
         std::cout << "Handler assigned" << std::endl;
-
-    }
+      }
 
     return 0;
 }
 
+//napraviti klasu korisnik
+//vektor kanala
+//vektor kanal tipa korisnik
+/*
+void connection_handler_general(vector<vector<Korisnik> > kanali) {
+
+}
+*/
 /*
  * This will handle connection for each client
  * */
-void *connection_handler(void *socket_desc)
-{
+void *connection_handler(void *socket_desc) {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size, j;
-    char *message , client_message[2000];
-    char username[666];
+    char *message , client_message[2000], username[666];
+    string imeKanala;
+    Korisnik *user;
+    Kanal general("General");
+    Kanal Programming("Programming");
+    Kanal Sex("Sex");
+    Kanal Kolaci("Kolaci");
 
     //Send some messages to the client
     message = "Unesite vas username: \n";
     write(sock , message , strlen(message));
-    recv(sock, username, 666, 0)
+    recv(sock, username, 666, 0);
+    user = new Korisnik(sock, username);
+    cout << "Korisnikov socket: " << user->getSocket() << endl;
+    cout << "Korisnikov username: "; user->printUsername();
+    //user->setSocket(sock);
+    //user->setUsername(username);
     message = "Izaberite kanal: \n1 - General\n2 - Programming\n3 - Sex\n4 - Kolaci\n";
     write(sock, message, strlen(message));
+    recv(sock, client_message, 2000, 0);
+    cout << "Poruka: " << client_message << endl;
+    int izbor = atoi(client_message);
+    cout << "Izbor: " << izbor << endl;
+    switch(izbor) {
+      case 1:
+        kanali.push_back(general);
+        kanali[0].dodajKorisnika(*user);
+        cout << "Kanal: " << kanali[0] << endl;
+        cout << kanali[0].getKorisnik()[0].printUsername() << endl;
+      break;
+      case 2:
+        //kanali[1].push_back(*user);
+      break;
+      case 3:
+      break;
+      case 4:
+      break;
+
+    }
     strcat(username, ":");
     strcat(username, " ");
     char disconnect[2666];
     char poruka[2666];
 
-    std::cout << "Username: " << username << std::endl;
 
-    //Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-    {
+    //std::cout << "Username: " << username << std::endl;
+    while( (read_size = recv(sock, client_message , 2000 , 0)) > 0 ) {
         client_message[read_size] = '\0';
         //Send the message back to client
         for (j = 0; j < i; j++) {
@@ -180,6 +291,7 @@ void *connection_handler(void *socket_desc)
           strcpy(disconnect, username);
           strcat(disconnect, "disconnected!");
           write(connected_client[j], disconnect, strlen(disconnect));
+          delete user;
         }
         fflush(stdout);
     }
